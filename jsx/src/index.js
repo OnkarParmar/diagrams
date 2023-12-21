e59bec44ec6b35feb3de63350303ae0943b262d0
Onkar Parmar <onkar@Onkars-MacBook-Pro.local>
2023-12-22 00:55:08 +0100

    clean up unnecessary files, basic jsx demo

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const home = ReactDOM.createRoot(document.getElementById('home'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+home.render(<App />);
+
+function App() {
+  let message = "Bye there!";
+  if (Math.random() > 0.5) {
+    message = "Hi There!";
+  }
+  
+  //return React.createElement("h3", null, message);      // using js
+  return <h3> {message} </h3>                             // using jsx
+}
